cbc0e21973d1fb1bce92cf62fa96777f191595a5
hc990 <hc990@github.com>
2026-04-20 18:56:16 +0800

    ci-cd v31

--- data/index.ts
+++ data/index.ts
@@ -1,16 +1,16 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+// import { PrismaClient } from '@prisma/client'
+// const prisma = new PrismaClient()
 
-async function main() {
-  const allUsers = await prisma.authors.findFirst({})
-  console.log(allUsers)
-}
+// async function main() {
+//   const allUsers = await prisma.authors.findFirst({})
+//   console.log(allUsers)
+// }
 
-main()
-  .catch(async (e) => {
-    console.error(e)
-    process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+// main()
+//   .catch(async (e) => {
+//     console.error(e)
+//     process.exit(1)
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect()
+//   })
